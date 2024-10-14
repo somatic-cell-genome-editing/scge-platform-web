16398c023bf8f8ca48fac7fd357b66f1567a2726
Thota <jthota@mcw.edu>
2024-10-14 11:24:27 -0500

    updated ct trials

--- src/main/webapp/js/search/ctFacets.js
+++ src/main/webapp/js/search/ctFacets.js
@@ -24,169 +24,29 @@ $(function () {
     }).on('hide.bs.collapse', function(){
         $(this).prev(".card-header").find(".fas").removeClass("fa-angle-down").addClass("fa-angle-up");
     });
-    $.each($('input[name="status"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-        console.log("SELECTED status :" + statusSelected);
-        if(statusSelected !== typeof undefined) {
-            $.each(statusSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsestatus').addClass('show')
-                }
-            })
-        }
-    });
-    $.each($('input[name="sponsor"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
+    var facets=["status","indication", "sponsor"
+        ,"sponsorClass", "therapyType", "vectorType",
+        "deliverySystem","routeOfAdministration","drugProductType","editorType",
+        "targetGeneOrVariant", "mechanismOfAction", "targetTissueOrCell", "phases","standardAges", "therapyRoute","locations" ]
+    $.each(facets, function (i,e){
+        var checkbox="\""+e+"\"";
+        var collapseId='#collapse'+e
+        console.log('e:'+e)
+        $.each($('input[name='+checkbox+']'), function(){
+            var _this=$(this);
+            var val=_this.val();
+            _this.prop('checked', false);
+            var selectedFacet=json[e]
+            console.log("SELECTED Facet :" + json[e]);
+            if(selectedFacet !== typeof undefined) {
+                $.each(selectedFacet, function (i, selected) {
+                    if (selected === val) {
+                        _this.prop('checked', true)
+                        $(collapseId).addClass('show')
+                    }
+                })
+            }
+        });
+    })
 
-        if(orgSelected !== typeof undefined) {
-            $.each(orgSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsesponsor').addClass('show')
-                }})}});
-    $.each($('input[name="indication"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(conditionSelected !== typeof undefined) {
-            $.each(conditionSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapseindication').addClass('show')
-                }
-            })
-        }
-    });
-    $.each($('input[name="trackerType"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(trackerTypeSelected !== typeof undefined) {
-            $.each(trackerTypeSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsetrackerType').addClass('show')
-                }
-            })
-        }
-    });
-
-
-    $.each($('input[name="vectorType"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(vectorTypeSelected !== typeof undefined) {
-            $.each(vectorTypeSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsevectorType').addClass('show')
-                }
-            })
-        }
-    });
-
-
-    $.each($('input[name="editorType"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(editorTypeSelected !== typeof undefined) {
-            $.each(editorTypeSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapseeditorType').addClass('show')
-                }
-            })
-        }
-    });
-
-
-    $.each($('input[name="deliverySystem"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(deliverySystemSelected !== typeof undefined) {
-            $.each(deliverySystemSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsedeliverySystem').addClass('show')
-                }
-            })
-        }
-    });
-
-
-
-    $.each($('input[name="drugProductType"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(drugProductTypeSelected !== typeof undefined) {
-            $.each(drugProductTypeSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsedrugProductType').addClass('show')
-                }
-            })
-        }
-    });
-
-
-    $.each($('input[name="sponsorClass"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(sponsorClasssSelected !== typeof undefined) {
-            $.each(sponsorClasssSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsesponsorType').addClass('show')
-                }
-            })
-        }
-    });
-
-
-    $.each($('input[name="therapyType"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(therapyTypeSelected !== typeof undefined) {
-            $.each(therapyTypeSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapsetherapyType').addClass('show')
-                }
-            })
-        }
-    });
-
-    $.each($('input[name="routeOfAdministration"]'), function(){
-        var _this=$(this);
-        var val=_this.val();
-        _this.prop('checked', false);
-
-        if(routeOfAdministrationSelected !== typeof undefined) {
-            $.each(routeOfAdministrationSelected, function (i, selected) {
-                if (selected === val) {
-                    _this.prop('checked', true)
-                    $('#collapserouteOfAdministration').addClass('show')
-                }
-            })
-        }
-    });
 })
