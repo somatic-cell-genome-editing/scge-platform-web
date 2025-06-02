baa1192c57dee84126c49b03a81993dfcd760625
Thota <jthota@mcw.edu>
2025-06-02 10:41:08 -0500

    upgraded to java 17
    security

--- src/main/java/edu/mcw/scge/configuration/SecurityConfiguration.java
+++ src/main/java/edu/mcw/scge/configuration/SecurityConfiguration.java
@@ -107,32 +107,32 @@ public class SecurityConfiguration {
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //    if(System.getenv("HOSTNAME")!=null && System.getenv("HOSTNAME").equals("localhost")) {
 //   //  if(!SCGEContext.isProduction() && !SCGEContext.isTest() && !SCGEContext.isDev()){
-//            http.authorizeHttpRequests(authorize->
-//                    authorize.requestMatchers("/**")
-//                            .permitAll());
+            http.authorizeHttpRequests(authorize->
+                    authorize.requestMatchers("/**")
+                            .permitAll());
 //    }else {
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/", "/home", "/logout", "/oauth_login", "/common/**", "/data/requestAccount", "/loginFailure",
-                        "/images/**", "/css/**", "/js/**", "/forms_public/**", "/data/**", "/login.jsp", "/login")
-                .permitAll()
-                .anyRequest().authenticated());
-
-               http. oauth2Login(auth2 -> auth2
-                    .loginPage("/dashboard").permitAll()
-                    .failureUrl("/loginFailure")
-                    .defaultSuccessUrl("/dashboard", true)
-                    .clientRegistrationRepository(clientRegistrationRepository())
-                    .authorizedClientService(authorizedClientService(clientRegistrationRepository()))
-                    .authorizationEndpoint(authorization -> authorization.baseUri("/login"))
-                    .tokenEndpoint(tokenEndpointConfig -> tokenEndpointConfig.accessTokenResponseClient(accessTokenResponseClient())))
-            .logout(logout -> logout
-                    .logoutRequestMatcher(PathPatternRequestMatcher.withDefaults().matcher("/logout"))
-                    .logoutSuccessUrl("/") // Redirect after logout
-                    .invalidateHttpSession(true) // Invalidate the session
-                    .deleteCookies("JSESSIONID") // Delete cookies
-            ).csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
-
-    http.headers(headers -> headers.cacheControl(HeadersConfigurer.CacheControlConfig::disable));
-//    }
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/", "/home", "/logout", "/oauth_login", "/common/**", "/data/requestAccount", "/loginFailure",
+//                        "/images/**", "/css/**", "/js/**", "/forms_public/**", "/data/**", "/login.jsp", "/login")
+//                .permitAll()
+//                .anyRequest().authenticated());
+//
+//               http. oauth2Login(auth2 -> auth2
+//                    .loginPage("/dashboard").permitAll()
+//                    .failureUrl("/loginFailure")
+//                    .defaultSuccessUrl("/dashboard", true)
+//                    .clientRegistrationRepository(clientRegistrationRepository())
+//                    .authorizedClientService(authorizedClientService(clientRegistrationRepository()))
+//                    .authorizationEndpoint(authorization -> authorization.baseUri("/login"))
+//                    .tokenEndpoint(tokenEndpointConfig -> tokenEndpointConfig.accessTokenResponseClient(accessTokenResponseClient())))
+//            .logout(logout -> logout
+//                    .logoutRequestMatcher(PathPatternRequestMatcher.withDefaults().matcher("/logout"))
+//                    .logoutSuccessUrl("/") // Redirect after logout
+//                    .invalidateHttpSession(true) // Invalidate the session
+//                    .deleteCookies("JSESSIONID") // Delete cookies
+//            ).csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+//
+//    http.headers(headers -> headers.cacheControl(HeadersConfigurer.CacheControlConfig::disable));
+////    }
     return http.build();
 }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,11 @@
-
-<%@include file="WEB-INF/jsp/header.jsp"%>
-<%@include file="WEB-INF/jsp/home.jsp"%>
-<%@include file="WEB-INF/jsp/footer.jsp"%>
+<%--<%@ page import="edu.mcw.scge.configuration.SecurityConfiguration" %>--%>
+<%--<%--%>
+<%--    if (SecurityConfiguration.REQUIRE_AUTHENTICATION) {--%>
+<%--%>--%>
+<%--    <jsp:include page="login.jsp" />--%>
+<%--<%--%>
+<%--    }else {--%>
+<%--%>--%>
+<%--<% request.setAttribute("page", "WEB-INF/jsp/home");%>--%>
+<%response.sendRedirect("home");%>
+<%--<% } %>--%>
